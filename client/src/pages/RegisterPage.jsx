356558bc5fd68d13c5903a4db1d685a02591b41c
dishant <aryadishant414@gmail.com>
2024-09-19 09:52:49 +0530

    Registration page UI completed and stored form data in React State Variable

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,7 +1,101 @@
+import { useState } from "react";
+
 export const RegisterPage = () => {
+    const[user, setUser] = useState({
+        username: "",
+        email: "",
+        phone: "",
+        password: "",
+    });
+
+    const handleInput = (e) => {
+        console.log("INPUT HAS BEEN CHNAGED : " , e);
+        let name = e.target.name;
+        let value = e.target.value;
+    
+        setUser({
+          ...user,
+          [name]: value,
+        });
+    };
+    
+    // handle form on submit
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+    };
+
+
+
     return (
         <>
-            <h1>Welcome to Register Page</h1>
+            <section>
+                <main>
+                    <div className="section-registration">
+                        <div className="container grid grid-two-cols">
+                            <div className="registration-image reg-img">
+                                <img
+                                src="/images/register.png"
+                                alt="a nurse with a cute look"
+                                width="400"
+                                height="500"
+                                />
+                            </div>
+                            {/* our main registration code  */}
+                            <div className="registration-form">
+                                <h1 className="main-heading mb-3">registration form</h1>
+                                <br />
+                                <form onSubmit={handleSubmit}>
+                                    <div>
+                                        <label htmlFor="username">username</label>
+                                        <input
+                                        type="text"
+                                        name="username"
+                                        value={user.username}
+                                        onChange={handleInput}
+                                        placeholder="enter your username"
+                                        />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="email">email</label>
+                                        <input
+                                        type="text"
+                                        name="email"
+                                        value={user.email}
+                                        onChange={handleInput}
+                                        placeholder="enter your email"
+                                        />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="phone">phone</label>
+                                        <input
+                                        type="number"
+                                        name="phone"
+                                        value={user.phone}
+                                        onChange={handleInput}
+                                        placeholder="enter your phone"
+                                        />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="password">password</label>
+                                        <input
+                                        type="password"
+                                        name="password"
+                                        value={user.password}
+                                        onChange={handleInput}
+                                        placeholder="enter your password"
+                                        />
+                                    </div>
+                                    <br />
+                                    <button type="submit" className="btn btn-submit">
+                                        Register Now
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </section>
         </>
     )
 }
