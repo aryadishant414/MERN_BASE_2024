345b369c5a74a88ca64bb3b768e54d783d7bcff5
dishant <aryadishant414@gmail.com>
2024-09-20 10:24:28 +0530

    Contact page done

--- server/models/contact-model.js
+++ server/models/contact-model.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const contactSchema = new mongoose.Schema(
     {
-        name: {
+        username: {
             type: String,
             required: true,
         },
